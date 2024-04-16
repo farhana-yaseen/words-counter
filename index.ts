ef31c1edcca7cf4d8af9f8ff12f8a4b86b05468d
farhana-yaseen <164941355+farhana-yaseen@users.noreply.github.com>
2024-04-16 16:11:15 -0500

    create word counter

---
+++ index.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+let condition = true;
+
+console.log(chalk.bgBlue.bold(" \n\t Word Counter \n" ));
+
+while (condition){
+
+const answer = await inquirer.prompt(
+    {
+    name: "sentence",
+    type: "input",
+    message: "Enter sentences for word counting: "
+    }
+)
+
+//use trim for delete white space and split for words convert in array
+
+const word = answer.sentence.trim().split(" ");
+
+console.log(word);
+
+console.log(chalk.bgBlue.bold(`\n Total word count is : ${word.length} \n`));
+
+const count = await inquirer.prompt(
+    {
+    name: "countAgain",
+    type: "confirm",
+    message: "Do you want to count again?",
+    default: false
+    }
+)
+condition = count.countAgain;
+}
